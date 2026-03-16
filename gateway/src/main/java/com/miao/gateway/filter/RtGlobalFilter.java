df13530786f508700d0fc63fd47abcc87e2635f8
xinyu-hou <xinyu-hou@qq.com>
2026-03-16 18:15:03 +0800

    feat:(gateway)使用全局过滤器，统计请求时间

---
+++ gateway/src/main/java/com/miao/gateway/filter/RtGlobalFilter.java
@@ -0,0 +1,42 @@
+package com.miao.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class RtGlobalFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        String uri = request.getURI().toString();
+        long start = System.currentTimeMillis();
+        log.info("请求【{}】开始：时间：{}",uri,start);
+        //========================以上是前置逻辑=========================
+
+        // mono是一个响应流
+        Mono<Void> filter = chain.filter(exchange)
+                .doFinally((result)->{ // 响应式api,会在响应后执行
+                    //=======================以下是后置逻辑=========================
+                    long end = System.currentTimeMillis();
+                    log.info("请求【{}】结束：时间：{}，耗时：{}ms",uri,end,end-start);
+                }); //放行   10s
+        // 下边写的代码会立刻执行，因为上行是异步执行的
+
+
+        return filter;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
